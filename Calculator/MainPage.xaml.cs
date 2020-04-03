3eda9720289db8c05f7af93262157af66d38e491
yuriy <your_email@whatever.com>
2020-04-03 17:25:28 +0300

    Логика калькулятора

--- Calculator/MainPage.xaml.cs
+++ Calculator/MainPage.xaml.cs
@@ -8,14 +8,73 @@ using Xamarin.Forms;
 
 namespace Calculator
 {
-	// Learn more about making custom code visible in the Xamarin.Forms previewer
-	// by visiting https://aka.ms/xamarinforms-previewer
-	[DesignTimeVisible(false)]
 	public partial class MainPage : ContentPage
 	{
+		int currentState = 1;
+		string mathOperator;
+		double firstNumber, secondNumber;
+
 		public MainPage()
 		{
 			InitializeComponent();
+			OnClear(this, null);
+		}
+
+		void OnSelectNumber(object sender, EventArgs e)
+		{
+			Button button = (Button)sender;
+			string pressed = button.Text;
+
+			if (this.resultText.Text == "0" || currentState < 0)
+			{
+				this.resultText.Text = "";
+				if (currentState < 0)
+					currentState *= -1;
+			}
+
+			this.resultText.Text += pressed;
+
+			double number;
+			if (double.TryParse(this.resultText.Text, out number))
+			{
+				this.resultText.Text = number.ToString("N0");
+				if (currentState == 1)
+				{
+					firstNumber = number;
+				}
+				else
+				{
+					secondNumber = number;
+				}
+			}
+		}
+
+		void OnSelectOperator(object sender, EventArgs e)
+		{
+			currentState = -2;
+			Button button = (Button)sender;
+			string pressed = button.Text;
+			mathOperator = pressed;
+		}
+
+		void OnClear(object sender, EventArgs e)
+		{
+			firstNumber = 0;
+			secondNumber = 0;
+			currentState = 1;
+			this.resultText.Text = "0";
+		}
+
+		void OnCalculate(object sender, EventArgs e)
+		{
+			if (currentState == 2)
+			{
+				var result = SimpleCalculator.Calculate(firstNumber, secondNumber, mathOperator);
+
+				this.resultText.Text = result.ToString();
+				firstNumber = result;
+				currentState = -1;
+			}
 		}
 	}
 }
